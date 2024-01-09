ca4e444cadea567a9ba0c12b74031a2d60973f25
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-09 15:22:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+var a = prompt("enter first variable");
+var b = a*2;
+var c = prompt("enter second variable");
+var d = parseInt(b)+parseInt(c);
+alert("The final result is: " +d);
